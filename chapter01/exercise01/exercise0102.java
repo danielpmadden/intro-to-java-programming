41a2a02dde2f3341bc0cd7a8448b4b9d8a87e3c9
Daniel Madden <44127480+danielpmadden@users.noreply.github.com>
2022-04-20 00:12:51 -0700

    Add files via upload

---
+++ chapter01/exercise01/exercise0102.java
@@ -0,0 +1,17 @@
+/*
+ * Chapter 1, Exercise 1.2, Introduction to Java Programming (10th).
+ * 
+ * (Display five messages) Write a program that displays Welcome to Java five times.
+ */
+
+public class exercise0102 
+{
+	public static void main(String[] args) 
+    {
+		System.out.println("Welcome to Java!");
+		System.out.println("Welcome to Java!");
+		System.out.println("Welcome to Java!");
+		System.out.println("Welcome to Java!");
+		System.out.println("Welcome to Java!");
+	}
+}
